c12b0a1115ad59a08cd85647c98763c89f1605ff
dickjuma <108567637+dickjuma@users.noreply.github.com>
2024-06-12 11:39:44 +0300

    Add files via upload

---
+++ school/add.php
@@ -0,0 +1,26 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="ss.php" method="POST">
+
+   <label for="">student id</label> <input type="text" name="studentid" id="">
+   <br>
+    <label for="">firstname</label><input type="text"name="first">
+    <br>
+    <label for="">lastname</label><input type="text" name="last">
+    <br>
+    <label for="">Eamil address</label><input type="text" name="email">
+    <br>
+    <input type="submit" value="ADD">
+    </form>
+</body>
+</html>
+
